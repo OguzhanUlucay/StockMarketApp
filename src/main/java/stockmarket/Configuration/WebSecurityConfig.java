9fb0f0c0739fd125648d78248e8a1463fcb21dfe
oguzhan ulucay <oguzhanulucay@gmail.com>
2019-08-15 00:02:03 +0300

    HTTP Basic authentication enabled for all endpoints.

---
+++ src/main/java/stockmarket/Configuration/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package main.java.stockmarket.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //HTTP Basic authentication
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/shares").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/shares").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/shares/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/shares/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/shares/**").hasRole("ADMIN")     
+                .antMatchers(HttpMethod.GET, "/sharesBook").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/sharesBook/**").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/sharesBook/**").hasRole("ADMIN")       
+                .antMatchers(HttpMethod.GET, "/users").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/users/{userId}").hasRole("USER")
+                .antMatchers(HttpMethod.PUT, "/users/{userId}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/users/{userId}").hasRole("ADMIN")              
+                .antMatchers(HttpMethod.GET, "/users/{userId}/shares").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/users/{userId}/shares/{shareCode}").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/users/{userId}/shares/{shareCode}").hasRole("USER")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
+	
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+
+    }
+
+}
